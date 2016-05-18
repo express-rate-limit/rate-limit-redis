5ba736abfeb1bcbc060bb2ad2e1e246823c2ac8f
Wyatt Johnson <wyattjoh@gmail.com>
2016-05-18 12:39:46 -0600

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,125 @@
+/*global describe, it */
+var RedisStore = require('../lib/redis-store.js');
+
+describe("rate-limit-redis node module", function() {
+
+  var MockRedisClient = function() {
+    var keys = {};
+
+    this.incr = function(key, cb) {
+      if (keys[key]) {
+        keys[key]++;
+      } else {
+        keys[key] = 1;
+      }
+
+      cb(null, keys[key]);
+    };
+
+    this.expire = function(key, expiry) {
+      setTimeout(function() {
+        delete keys[key];
+      }, expiry * 1000);
+    };
+
+    this.del = function(key) {
+      delete keys[key];
+    };
+  };
+
+  it("can be created without error", function(done) {
+    try {
+      new RedisStore({
+        client: new MockRedisClient()
+      });
+    } catch (e) {
+      return done(e);
+    }
+
+    done();
+  });
+
+  it("sets the value to 1 on first incr", function(done) {
+    var store = new RedisStore({
+      client: new MockRedisClient()
+    });
+    var key = "test-store-incr-first";
+
+    store.incr(key, function(err, value) {
+      if (err) {
+        done(err);
+      } else {
+        if (value === 1) {
+          done();
+        } else {
+          done(new Error("incr did not set the key on the store to 1, was set to " + value));
+        }
+      }
+    });
+  });
+
+  it("increments the key for the store each incr", function(done) {
+    var store = new RedisStore({
+      client: new MockRedisClient()
+    });
+    var key = "test-store-incr";
+
+    store.incr(key, function() {
+      store.incr(key, function(err, value) {
+        if (err) {
+          done(err);
+        } else {
+          if (value === 2) {
+            done();
+          } else {
+            done(new Error("incr did not increment the store"));
+          }
+        }
+      });
+    });
+  });
+
+  it("resets the key for the store when used with resetKey", function(done) {
+    var store = new RedisStore({
+      client: new MockRedisClient()
+    });
+    var key = "test-store-resetKey";
+
+    store.incr(key, function() {
+      // value should be 1 now
+      store.resetKey(key);
+      // value should be 0 now
+      store.incr(key, function(err, value) {
+        // value should be 1 now
+        if (value === 1) {
+          done();
+        } else {
+          done(new Error("resetKey did not reset the store for the key provided"));
+        }
+      });
+    });
+  });
+
+  it("resets all keys for the store when the timeout is reached", function(done) {
+    var store = new RedisStore({
+      client: new MockRedisClient(),
+      expiry: 1
+    });
+    var key = "test-store-timeout";
+
+    store.incr(key, function() {
+      // valueOne should be 1 now
+      setTimeout(function() {
+        // valueOne and valueTwo should be 0 now
+        store.incr(key, function(err, value) {
+          // valueOne should be 1 now
+          if (value === 1) {
+            done();
+          } else {
+            done(new Error("reaching the timeout did not reset the key in the store"));
+          }
+        });
+      }, 1500);
+    });
+  });
+});
